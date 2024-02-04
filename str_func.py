900d5505122e73b6417344896606e7198f7e090b
Evgenij Cherednichenko <Ch_evgenij@mail.ru>
2024-02-04 04:58:46 +0300

    Решился конфликт файла

--- str_func.py
+++ str_func.py
@@ -3,5 +3,11 @@ def registr():
     word = input().upper()
     return word
 
+def registr_title():
+    """Возвращает заглавные первые буквы каждого слова в строке"""
+    word = input().title()
+    return word
+
 
 print(registr())
+print(registr_title())
